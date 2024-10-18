188a0560be95b8f0126f4e08cfbbf18cd13fa139
aaranguren-uni <aaranguren@uni.eus>
2024-10-18 10:55:31 +0200

    Release 0.0.1
    * OOP - Aldaketak igo
    
    * OOP - Git ignore

---
+++ OOP/Froga.py
@@ -0,0 +1,12 @@
+import Gelatina
+import Etxetresnak
+
+gel1 = Gelatina.Gelatina("handia", "gorria", "marrubia")
+gel1.sortu()
+
+mug= Etxetresnak.Mugikorra("Iphone 7","IOS")
+mug.piztu()
+mug.deitu()
+
+garb=Etxetresnak.Garbigailua("LG",[40, 70])
+garb.getGarbitzeTenperaturak()
---
+++ OOP/Gelatina.py
@@ -0,0 +1,8 @@
+class Gelatina:
+    def __init__(self, tam, kolore, zapore):
+        self.tam = tam
+        self.kolore = kolore
+        self.zapore = zapore
+    
+    def sortu(self):
+        print(f"Zure gelatinaren tamaina {self.tam}, kolorea {self.kolore} eta zaporea {self.zapore}")
